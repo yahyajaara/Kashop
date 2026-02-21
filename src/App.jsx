d8c5d80939e38cc686c47dda69309051a3e83cfb
yahyajaara <yahyajaara1411@gmail.com>
2026-02-21 07:30:30 +0200

    edit App.jsx2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import router from './Router'
+import router from './route'
 
 export default class App extends Component {
   render() {
